f0c0f650ebf2c0b8bf82e8611c25a95e6dec8ed4
@felix.puck <felix.puck@telekom.de>
2024-01-23 23:11:14 +0100

    First Changes

---
+++ my-app/src/MainContent.jsx
@@ -0,0 +1,13 @@
+import {Component} from "react";
+
+
+export default class MainContent extends Component
+{
+    render() {
+        return(
+            <div>
+                <h1 className="text-font">Hi im the Main Content</h1>
+            </div>
+        )
+    }
+}
